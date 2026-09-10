d7bc289499eb01d4714de7052e81313c33b074a3
planb2dream <planb2dream@gmail.com>
2026-09-11 01:57:37 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'attendance-tracker-v21-cosmos-atmosphere-percentage';
+const CACHE = 'attendance-tracker-v22-cosmos-safe-bottom';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
